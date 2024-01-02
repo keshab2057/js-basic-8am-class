cab0e1c18219ce96bd2529004c433e6eba32bc3b
keshab sutar <keshabsingh981829@gmail.com>
2024-01-02 22:14:27 +0545

    defining variables

--- js/variables/index.js
+++ js/variables/index.js
@@ -1,1 +1,31 @@
 console.log("hello world");
+
+//hello world(singhle line comment)
+// multiline hosting
+/*
+Readme comment
+#variables-->name storage
+=>standard ECMASCRIPT
+es5-->var
+es6-->var,let,const
+
+
+a
+b 
+c
+ */
+// hello keshab
+// console.log("keshab")
+var person = "keshab";
+var fullName = "kshab sutar";
+var isAdult = "yes";
+let person = "keshab";
+const personName = "aakash";
+const phone = "234567";
+
+//var,let--> data replace
+//const-->constant variable
+
+//Hoisting-->variable lai junsukai tahuma declare garepani run gard kheri first ma aaux so called as Hoisting.
+
+console.log("person");
