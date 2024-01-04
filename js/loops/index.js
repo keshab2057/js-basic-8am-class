01ff9e21cc4d2bdfb8b945502916f66f9a4f42cf
keshab sutar <keshabsingh981829@gmail.com>
2024-01-04 14:49:47 +0545

    do while loop program

--- js/loops/index.js
+++ js/loops/index.js
@@ -1,1 +1,9 @@
 console.log("hello world");
+
+// do while loop-->syntax=>do{code execute}while(condition)
+let i = 1;
+do {
+  console.log(i);
+  i++;
+} while (i <= 100);
+
--- js/variables/index.js
+++ js/variables/index.js
@@ -14,9 +14,4 @@ console.log("hello world");
 //     i++;
 // }
 
-// do while loop-->syntax=>do{code execute}while(condition)
-// let i = 1;
-// do {
-//   console.log(i);
-//   i++;
-// } while (i <= 100);
+/
