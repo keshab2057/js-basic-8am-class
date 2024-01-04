eb3533066ea39a22e1a7a8136e0926bb45eb33e6
keshab sutar <keshabsingh981829@gmail.com>
2024-01-04 14:42:55 +0545

    loops syntax set up

--- js/variables/index.js
+++ js/variables/index.js
@@ -1,22 +1,22 @@
 console.log("hello world");
 
-//loops
-//for loops-->syntax=>for(intialization;conditions;incrment,decrement){//code execute}
-//wap to print 1 to 100
+// loops
+// for loops-->syntax=>for(intialization;conditions;incrment,decrement){//code execute}
+// wap to print 1 to 100
 
 // for(let i = 1; i<=100; i++){
 //     console.log(i)
 // }
-//while loops-->syntax=>while(conditions){code execute}
+// while loops-->syntax=>while(conditions){code execute}
 // let i = 1;
 // while(i<=100){
 //     console.log(i);
 //     i++;
 // }
 
-//do while loop-->syntax=>do{code execute}while(condition)
-let i = 1;
-do {
-  console.log(i);
-  i++;
-} while (i <= 100);
+// do while loop-->syntax=>do{code execute}while(condition)
+// let i = 1;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 100);
