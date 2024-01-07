052b989199589ab256acc2ef71b74398918783bb
keshab sutar <keshabsingh981829@gmail.com>
2024-01-07 21:23:20 +0545

    simple interactons set up

--- js/function/index.js
+++ js/function/index.js
@@ -1,13 +1,19 @@
 console.log("Hello function chapter");
 
-//check the function is even or odd
-function check(number) {
-  const result = number % 2;
-  if (result === 0) {
-    console.log("even");
-  } else {
-    console.log("odd");
-  }
+// //check the function is even or odd
+// function check(number) {
+//   const result = number % 2;
+//   if (result === 0) {
+//     console.log("even");
+//   } else {
+//     console.log("odd");
+//   }
+// }
+// //call/run
+// check(); //default odd print
+
+//write a program to print even numbers 0 to 100;
+let i = 0;
+while(i <= 100){
+console.log(i);
 }
-//call/run
-check(); //default odd print
