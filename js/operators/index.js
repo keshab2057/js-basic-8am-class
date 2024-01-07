85694d7fb65e30c07177e3c583c0128cd77f2c32
keshab sutar <keshabsingh981829@gmail.com>
2024-01-07 21:20:42 +0545

    simple project interactions

--- js/operators/index.js
+++ js/operators/index.js
@@ -1,96 +1,21 @@
 console.log("Hello operators schapter");
-//operators
-// arithmatic operaters:+,-,*,%,modulus,exponentiation,increment,decrement
+
+alert("Good morning keshab");
+const age = Number(prompt("How old are you?"));
+console.log(age);
+const isValid = confirm("Are you above 18?");
+console.log(isValid);
 
 
-// let a = 5;
-// let b = 2; 
-// result = a+b;
-// console.log(result);or
-// console.log("a+b=",a+b);
+//write a program that store the information about their birth year and calculate age.
 
-//modulus-->shows the remainder
-// console.log("a%b=", a%b);
-
-//exponentiation
-// console.log("a ** b",a ** b);
-
-//increment-->a++(post increment),++a(pre increment)
-// let a = 5;
-// console.log("a++ =", a++);
-// console.log("a =", a);       increment
-// console.log("++a =", ++a);
-
-// console.log("a-- =", a--);
-// console.log("a =", a);      decrement
-// console.log("--a =", --a);
-
-
-
-//Assignment operators-->=,+=,-=,*=,%=,**=
-
-// let a = 5;
-// let b = 2; 
-// a += 4;//a = a+4;
-// console.log("a=",a);
-
-// let a = 5;
-// let b = 2; 
-// a *= 5;//a= 5*5
-// console.log("a =", a); 
-
-// a %= 3;//a= 5%3-->show remainder
-// console.log("a =", a);
-
-// a **= 2;//a= 5*5
-// console.log("a =", a);
-
-
-//comparison operators--> ==(equal to),===(equal to and type(string,int or other)),<=,>=,!=(not equal to),!==(not equal and type)
-
-//equal to
-// let a = 3;
-// let b = 3;
-// console.log("a == b",a==b);
-
-
-// equal to and type(===)
-// let a = 3;
-// let b = "3";
-// console.log("a === b",a===b);
-
-//not equal to
-
-// let a = 3;
-// let b = 2;
-// console.log("3 != 2",a != b);/true
-
-
-
-//not equal to and type(===)
-
-// let a = 3;
-// let b = "3";
-// console.log("3 !== 3",a !== b);/true
-
-
-//logical operators-->logical AND &&,Logical OR, logical NOT
-
-//logical AND
-// let a = 5;
-// let b = 4;
-// let cond1 = a>b;//true
-// let cond2 = a ===4;//true
-// console.log("cond1 && cond2=", cond1 && cond2);
-
-//logical not
-// let a = 7;
-// console.log("a =",  a != 6);
-
-//logical OR
-// let a = 4;
-// let b = 7;
-// let cond1 = a == b;
-// let cond2 =  b > a;
-// console.log("cond1 || cond2=", cond1 || cond2);
-
+// const year = Number(prompt("what is birth year?"));
+// console.log(year);
+// const age = 2024 - year;
+// console.log(age);
+// //age>18-->to print adult \\age<18-->to print teeners
+// if(age < 18){
+//     alert("you are teeners");
+// }else{
+//     alert("you are adult");
+// }
