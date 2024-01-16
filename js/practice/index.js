cc0a8dda8cea23a41d4cf5e1eef1c28d6ff7af24
keshab sutar <keshabsingh981829@gmail.com>
2024-01-16 15:58:02 +0545

    bootstrap html,css,js basic concept

--- js/practice/index.js
+++ js/practice/index.js
@@ -1,13 +1,12 @@
 //date and date properties
-const today = new Date();
-// const today = new Date();-->recently date find out-->new Date
-// console.log(today); //output-->2024-01-14
+// const today = new Date();//-->recently date find out-->new Date
+// console.log( today); //output-->2024-01-14
 
 
 // const year = today.toString();-->above date is convert into string
 // console.log(year);//ouput-->sun,14,jan,2024(this output is string)
 
-// const myToday = new Date("2023-05-12");-->self date set garna sakxau
+// const myToday = new Date("2023-05-29");//-->self date set garna sakxau
 // console.log(myToday);//output-->2023-05-12
 
 // const date = today.getDate();
@@ -17,8 +16,8 @@ const today = new Date();
 // const minute = today.getMinutes();
 // console.log(minute);--->recent minutes show
 
-const settoday = today.setFullYear(2023);
-console.log(settoday);
+// const settoday = today.setFullYear(2023);
+// console.log(settoday);
 //date formats
 //UTC=>TZ(time zone)
 //ISO=>TZ+00:00
